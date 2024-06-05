a19a2eb033341b665688c7129a6de1c548d5d194
Snezhana Petlova <snpetlova@gmail.com>
2024-06-05 15:15:57 +0200

    add new record

--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { useUser } from "@clerk/clerk-react";
-// import { useFinancialRecords } from "../../contexts/financial-record-context";
+import { useFinancialRecords } from "../../contexts/financial-record-context";
 
 export const FinancialRecordForm = () => {
   const [description, setDescription] = useState<string>("");
   const [amount, setAmount] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [paymentMethod, setPaymentMethod] = useState<string>("");
-//   const { addRecord } = useFinancialRecords();
+  const { addRecord } = useFinancialRecords();
 
   const { user } = useUser();
 
@@ -23,7 +23,7 @@ export const FinancialRecordForm = () => {
       paymentMethod: paymentMethod,
     };
 
-    // addRecord(newRecord);
+    addRecord(newRecord);
     setDescription("");
     setAmount("");
     setCategory("");
