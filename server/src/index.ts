f6c624417a622ac7f16fc6ec990bf1dd4476665a
Snezhana Petlova <snpetlova@gmail.com>
2024-06-05 13:04:26 +0200

    get and post req

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
@@ -13,6 +14,8 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log("Connected to database!"))
   .catch((err) => console.error("Failed to connect to database!", err));
+
+app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
